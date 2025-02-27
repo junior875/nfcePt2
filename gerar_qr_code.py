32bab8818fee8386f617b55edfd88fa8f657d73d
junior875 <eduardojrizidoro@gmail.com>
2025-02-27 16:34:04 -0300

    gerador de qr code teste

---
+++ gerar_qr_code.py
@@ -0,0 +1,18 @@
+import qrcode
+
+# Informações da NFC-e
+chave_acesso = "31250248144666000140650010000010021987654329"  # Chave de Acesso da NFC-e
+versao_qrcode = "2"  # Versão do QR Code
+ambiente = "1"  # 1 para Produção, 2 para Homologação
+csc = "SEU_CODIGO_CSC"  # Código de Segurança do Contribuinte (CSC)
+id_token = "ID_TOKEN"  # Identificador do CSC
+
+# Montando a URL do QR Code
+dados_qrcode = f"{chave_acesso}|{versao_qrcode}|{ambiente}|{csc}|{id_token}"
+url_qrcode = f"https://portalsped.fazenda.mg.gov.br/portalnfce/sistema/qrcode.xhtml?p={dados_qrcode}"
+
+# Gerando o QR Code
+qr = qrcode.make(url_qrcode)
+qr.save("qrcode_nfce.png")
+
+print("QR Code gerado com sucesso! Verifique o arquivo 'qrcode_nfce.png'.")
